784c15f3a29bcf8c186101d0c7b082593eb87ed1
gyala825 <168166795+gyala825@users.noreply.github.com>
2024-10-18 19:46:33 +0800

    Update repr_net.py

--- repr_net.py
+++ repr_net.py
@@ -1,9 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from net.net_utils import BasicConv1d
-from net.CA import CoordAtt
-from net.tcn import TemporalConvNet
+from net_utils import BasicConv1d
+from tcn import TemporalConvNet
     
 def norm(x):
     out = F.normalize(x - x.mean(dim=-1, keepdim=True), 2, dim=-1)
@@ -16,7 +15,6 @@ class stem(nn.Module):
             BasicConv1d(channels, channels, 1),
             BasicConv1d(channels, channels, 3)
         )
-        # self.bn = nn.BatchNorm1d(channels)   #多余 pth删参数
         self.bn1 = nn.BatchNorm1d(channels)
         self.branch_1 = nn.Sequential(
             BasicConv1d(channels, channels // 2, 3),
@@ -47,7 +45,6 @@ class pssp_repr(nn.Module):
         self.conv = BasicConv1d(num_channels, 96, kernel_size=9)
         self.fc = nn.Linear(96, 32, bias=False)
         self.dropout = nn.Dropout(dropout)
-        # self.ca = CoordAtt(num_channels) 
     
     def forward(self, x):
         x = self.dropout(x)
@@ -58,9 +55,3 @@ class pssp_repr(nn.Module):
         return x.transpose(1,2)
 #     x [b, 32, L]
     
-
-if __name__ == '__main__':
-    model = pssp_repr(1280)
-    input_data = torch.randn(32, 1280, 20)
-    out = model(input_data)
-    print(out.shape)
